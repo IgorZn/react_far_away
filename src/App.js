1ca915a3dbee18035743270ebd8bfa9beadbed18
IgorZn <igor.znamensky@gmail.com>
2023-11-16 10:02:43 +0300

    Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
             <Logo/>
             <Form setItems={setObj}/>
             <PackingList items={obj} setItems={setObj}/>
-            <Stats />
+            <Stats items={obj}/>
         </div>
     );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
 function Stats(props) {
+    const { items } = props
+
+    if(!items.length)
+        return (
+            <p className={"stats"}>
+                <em className={"display-5"}>Start adding items to your packing list 🚀</em>
+            </p>
+        )
+
+    const getPackedPercent = () => {
+        const packed = items.filter(item => item.packed === true)
+        const percents = packed.length / items.length  * 100
+        return Math.floor(percents)
+    }
+
+    const packed = items.filter(item => item.packed === true)
+
     return (
         <div className={"stats"}>
             <footer>
-                <em>💼 You have X items on your list, and you already packed X (x%)</em>
+                <em>{getPackedPercent() === 100 ? 'You got everything to go ✈️' :
+                `💼 You have ${items.length} items on your list, and you already packed ${packed.length} (${getPackedPercent()}%)`}
+                </em>
             </footer>
         </div>
 
